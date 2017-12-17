10395b67cc50305553930172cc928671cf5c6b2d
Ron Toland <rontoland@gmail.com>
2017-12-16 16:01:38 -0800

    no more marmalade, thanks

--- init.el
+++ init.el
@@ -1,8 +1,5 @@
 (require 'package)
 (add-to-list 'package-archives
-             '("marmalade" .
-               "https://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
 	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
